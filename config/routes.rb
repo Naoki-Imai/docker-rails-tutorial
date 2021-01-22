16bc22998567579485176880c502647f2c2cdc5d
Naoki Imai <naokinetin0110@gmail.com>
2021-01-22 21:52:18 +0900

    finish layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to:'user#new'
   root 'static_pages#index'
-  get 'static_pages/index'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/help', to:'static_pages#help'
+  get '/about', to:'static_pages#about'
+  get '/contact', to:'static_pages#contact'
 end
