54de7f983e0d2749417f9d8962dd48121a4b3560
Naoki Imai <naokinetin0110@gmail.com>
2021-01-24 13:16:58 +0900

    finish session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/signup', to:'users#new'
   root 'static_pages#index'
   get '/help', to:'static_pages#help'
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
   post '/signup', to: 'users#create'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
   resources :users
 end
